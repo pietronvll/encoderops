9c457b2b78803d1875c89d32da70d3d88e5cd973
Pietro Novelli <pietro.novelli@iit.it>
2025-04-11 15:29:27 +0200

    memory leak somewhere

--- scripts/to_lmdb.py
+++ scripts/to_lmdb.py
@@ -1,3 +1,4 @@
+import json
 import os
 import pickle
 from pathlib import Path
@@ -11,22 +12,35 @@ from tqdm import tqdm
 from src.data import mdtraj_load, traj_to_confs
 
 
-def main(cutoff: float = 7.0, system_selection: str = "all and not type H"):
-    data_path = Path(os.environ["CHIG_DATA_PATH"])
-    trajectory_files = [str(traj) for traj in data_path.glob("*.dcd")]
-    top = next(data_path.glob("*.pdb")).__str__()
-    name = next(data_path.glob("*.pdb")).stem
+def main(
+    protein_id: str,
+    traj_id: int = 0,
+    cutoff: float = 7.0,
+    system_selection: str = "all and not type H",
+):
+    data_path = Path(os.environ["DATA_PATH"])
+    protein_path = (
+        data_path
+        / f"DESRES-Trajectory_{protein_id}-{traj_id}-protein/{protein_id}-{traj_id}-protein"
+    )
+    trajectory_files = [str(traj) for traj in protein_path.glob("*.dcd")]
+    top = next(protein_path.glob("*.pdb")).__str__()
+    name = next(protein_path.glob("*.pdb")).stem
     traj = mdtraj_load(trajectory_files, top, 1)
-
     configs, z_table, atom_names = traj_to_confs(
         traj, system_selection=system_selection
     )
-
-    database_path = Path(__file__).parent.parent / "preprocessed_data"
+    metadata = {
+        "cutoff": cutoff,
+        "system_selection": system_selection,
+    }
+    database_path = protein_path.parent / "lmdb"
     logger.info(database_path.__str__())
     if not database_path.exists():
         database_path.mkdir(parents=True)
-    lmdb_path = database_path / f"{name}-cutoff{int(cutoff)}.lmdb"
+    metadata_path = database_path / "metadata.json"
+    json.dump(metadata, open(metadata_path, "w"))
+    lmdb_path = database_path / f"{name}.lmdb"
     map_size = 10_995_116_277_760  # 1 TB
     env = lmdb.open(lmdb_path.__str__(), map_size=map_size, subdir=False)
     store_to_lmdb(env, configs, z_table, cutoff)
@@ -45,8 +59,13 @@ def store_to_lmdb(env, configs, z_table, cutoff):
                 cutoff=cutoff,
                 buffer=0.0,
             )
+            pickled_value = pickle.dumps(value)
             item_key = f"item_{idx}".encode()
-            txn.put(item_key, pickle.dumps(value))
+            txn.put(item_key, pickled_value)
+
+            del value
+            del pickled_value
+
 
 
 if __name__ == "__main__":
--- src/data.py
+++ src/data.py
@@ -1,43 +1,15 @@
-from typing import Union
+import os
+import pickle
+from functools import lru_cache
 
+import lmdb
 import mdtraj
-import numpy as np
-import torch
-from mlcolvar.data import DictDataset
-from mlcolvar.data.graph.utils import create_dataset_from_configurations
 from mlcolvar.utils.io import (
     _configures_from_trajectory,
     _names_from_top,
     _z_table_from_top,
 )
-from mlcolvar.utils.timelagged import find_timelagged_configurations, tprime_evaluation
-
-from src.configs import DataArgs
-
-
-def get_dataset(
-    trajectory_files: list[str],
-    top: str,
-    data_args: DataArgs,
-):
-    traj = mdtraj_load(trajectory_files, top, data_args.lagtime)
-    # Create configs
-    configs, z_table, atom_names = traj_to_confs(
-        traj, system_selection=data_args.system_selection
-    )
-    # Create dataset (?)
-    dataset = create_dataset_from_configurations(
-        configs,
-        z_table,
-        data_args.cutoff,
-        0.0,
-        atom_names,
-        data_args.remove_isolated_nodes,
-        show_progress=True,
-    )
-    # Lagged dataset (?)
-    timelagged_dataset = create_timelagged_dataset(dataset, progress_bar=False)
-    return timelagged_dataset
+from torch.utils.data import Dataset
 
 
 def traj_to_confs(traj: mdtraj.Trajectory, system_selection: str | None = None):
@@ -53,143 +25,43 @@ def mdtraj_load(trajectory_files: list[str], top: str, stride: int = 1000):
     return traj
 
 
-def create_timelagged_dataset(
-    X: Union[torch.Tensor, np.ndarray, DictDataset],
-    t: torch.Tensor = None,
-    lag_time: float = 1,
-    reweight_mode: str = None,
-    logweights: torch.Tensor = None,
-    tprime: torch.Tensor = None,
-    interval: list = None,
-    progress_bar: bool = False,
-):
-    """
-    Create a DictDataset of time-lagged configurations.
-
-    In case of biased simulations the reweight can be performed in two different ways (``reweight_mode``):
-
-    1. ``rescale_time`` : the search for time-lagged pairs is performed in the accelerated time (dt' = dt*exp(logweights)), as described in [1]_ .
-    2. ``weights_t`` : the weight of each pair of configurations (t,t+lag_time) depends only on time t (logweights(t)), as done in [2]_ , [3]_ .
-
-    If reweighting is None and tprime is given the `rescale_time` mode is used. If instead only the logweights are specified the user needs to choose the reweighting mode.
-
-    References
-    ----------
-    .. [1] Y. I. Yang and M. Parrinello, “Refining collective coordinates and improving free energy
-        representation in variational enhanced sampling,” JCTC 14, 2889–2894 (2018).
-    .. [2] J. McCarty and M. Parrinello, "A variational conformational dynamics approach to the selection
-        of collective variables in meta- dynamics,” JCP 147, 204109 (2017).
-    .. [3] H. Wu, et al. "Variational Koopman models: Slow collective variables and molecular kinetics
-        from short off-equilibrium simulations." JCP 146.15 (2017).
-
-    Parameters
-    ----------
-    X : array-like
-        input descriptors
-    t : array-like, optional
-        time series, by default np.arange(len(X))
-    reweight_mode: str, optional
-        how to do the reweighting, see documentation, by default none
-    lag_time: float, optional
-        lag between configurations, by default = 10
-    logweights : array-like,optional
-        logweight of each configuration (typically beta*bias)
-    tprime : array-like,optional
-        rescaled time estimated from the simulation. If not given and `reweighting_mode`=`rescale_time` then `tprime_evaluation(t,logweights)` is used
-    interval : list or np.array or tuple, optional
-        Range for slicing the returned dataset. Useful to work with batches of same sizes. Recall that with different lag_times one obtains different datasets, with different lengths
-    progress_bar: bool
-        Display progress bar with tqdm
-
-    Returns
-    -------
-    dataset: DictDataset
-        Dataset with keys 'data', 'data_lag' (data at time t and t+lag), 'weights', 'weights_lag' (weights at time t and t+lag).
-
-    """
-
-    # check reweigthing mode if logweights are given:
-    # 1) if rescaled time tprime is given
-    if tprime is not None:
-        if reweight_mode is None:
-            reweight_mode = "rescale_time"
-        elif reweight_mode != "rescale_time":
-            raise ValueError(
-                "The `reweighting_mode` needs to be equal to `rescale_time`, and not {reweight_mode} if the rescale time `tprime` is given."
-            )
-    # 2) if logweights are given
-    elif logweights is not None:
-        if reweight_mode is None:
-            reweight_mode = "rescale_time"
-            # TODO output warning or error if mode not specified?
-            # warnings.warn('`reweight_mode` not specified, setting it to `rescale_time`.')
-
-    # define time if not given
-    if t is None:
-        t = torch.arange(0, len(X))
-    else:
-        if len(t) != len(X):
-            raise ValueError(
-                f"The length of t ({len(t)}) is different from the one of X ({len(X)}) "
-            )
-
-    # define tprime if not given:
-    if reweight_mode == "rescale_time":
-        if tprime is None:
-            tprime = tprime_evaluation(t, logweights)
-    else:
-        tprime = t
-
-    # find pairs of configurations separated by lag_time
-    if isinstance(X, torch.Tensor) or isinstance(X, np.ndarray):
-        x_t, x_lag, w_t, w_lag = find_timelagged_configurations(
-            X,
-            tprime,
-            lag_time=lag_time,
-            logweights=logweights if reweight_mode == "weights_t" else None,
-            progress_bar=progress_bar,
+class DESRESDataset(Dataset):
+    def __init__(self, protein_id: str, traj_id: int = 0, lagtime: int = 1):
+        super().__init__()
+        dataset_path = (
+            os.environ("DATA_PATH")
+            / f"DESRES-Trajectory_{protein_id}-{traj_id}-protein/{protein_id}-{traj_id}-protein/lmdb/{protein_id}-{traj_id}-protein.lmdb"
         )
-    elif isinstance(X, DictDataset):
-        index = torch.arange(len(X), dtype=torch.long)
-        x_t, x_lag, w_t, w_lag = find_timelagged_configurations(
-            index,
-            tprime,
-            lag_time=lag_time,
-            logweights=logweights if reweight_mode == "weights_t" else None,
-            progress_bar=progress_bar,
-        )
+        assert dataset_path.exists()
+        map_size = 10_995_116_277_760  # 1 TB
+        self.env = lmdb.open(self.lmdb_path, map_size=map_size, subdir=False)
+        self.lagtime = lagtime
 
-    # return only a slice of the data (N. Pedrani)
-    if interval is not None:
-        # convert to a list
-        data = list(x_t, x_lag, w_t, w_lag)
-        # assert dimension of interval
-        assert len(interval) == 2
-        # modifies the content of data by slicing
-        for i in range(len(data)):
-            data[i] = data[i][interval[0] : interval[1]]
-        x_t, x_lag, w_t, w_lag = data
+    @lru_cache(maxsize=10000)  # Cache recently used items
+    def _get_lmdb_item(self, key, idx):
+        """Get a specific item from LMDB with caching"""
+        if self.env is None:
+            raise RuntimeError("LMDB environment not initialized")
 
-    if isinstance(X, torch.Tensor) or isinstance(X, np.ndarray):
-        dataset = DictDataset(
-            {"data": x_t, "data_lag": x_lag, "weights": w_t, "weights_lag": w_lag},
-            data_type="descriptors",
-        )
-        return dataset
+        item_key = f"{key}_{idx}".encode()
+        with self.env.begin(write=False) as txn:
+            data_binary = txn.get(item_key)
+            if data_binary is None:
+                raise KeyError(f"Key '{key}' at index {idx} not found in LMDB")
+            return pickle.loads(data_binary)
 
-    elif isinstance(X, DictDataset):
-        # we use deepcopy to avoid editing the original dataset
-        dataset = DictDataset(
-            dictionary={
-                "data_list": X[x_t.numpy().tolist()]["data_list"],
-                "data_list_lag": X[x_lag.numpy().tolist()]["data_list"],
-            },
-            metadata={"z_table": X.metadata["z_table"], "cutoff": X.metadata["cutoff"]},
-            data_type="graphs",
-        )
-        # update weights
-        for i in range(len(dataset)):
-            dataset["data_list"][i]["weight"] = w_t[i]
-            dataset["data_list_lag"][i]["weight"] = w_lag[i]
+    def __getitem__(self, index):
+        if isinstance(index, str):
+            return self._dictionary[index]
+        else:
+            slice_dict = {}
+            for key, val in self._dictionary.items():
+                try:
+                    slice_dict[key] = val[index]
+                except:
+                    slice_dict[key] = list(itemgetter(*index)(val))
+            return slice_dict
 
-        return dataset
+    def __len__(self):
+        value = next(iter(self._dictionary.values()))
+        return len(value)
