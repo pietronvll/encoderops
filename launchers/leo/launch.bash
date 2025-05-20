054d176be1ffdaeef34df02af58cfff84de0e9d9
Pietro Novelli <pietronvll@gmail.com>
2025-05-20 15:45:36 +0200

    Tiny stuf

--- launchers/leo/launch.bash
+++ launchers/leo/launch.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --account=IscrC_LR4LSDS        # project name
+#SBATCH --account=IscrC_LR4LSDS     # project name
 #SBATCH --partition=boost_usr_prod  # partition to be used
-#SBATCH --time 4:00:00             # format: HH:MM:SS
+#SBATCH --time 4:00:00              # format: HH:MM:SS
 #SBATCH --nodes=1                   # node
 #SBATCH --ntasks-per-node=4         # tasks out of 32
 #SBATCH --gres=gpu:4                # gpus per node out of 4
