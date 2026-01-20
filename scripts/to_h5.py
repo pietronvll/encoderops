d4b90009134463d2a657b9b5a046a828091deb60
Giacomo Turri <giacomo.turri@iit.it>
2026-01-20 23:05:46 +0100

    Fix h5 files storage

--- scripts/to_h5.py
+++ scripts/to_h5.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 from pathlib import Path
 
 import h5py
@@ -6,6 +7,7 @@ import numpy as np
 import torch
 import tyro
 from loguru import logger
+from tqdm import tqdm
 
 from src.data import mdtraj_load, traj_to_confs
 
@@ -43,17 +45,51 @@ def main(
 
 @torch.no_grad()
 def store_to_h5(h5_path, configs, z_table, metadata):
-    with h5py.File(h5_path, "w") as f:
-        # Store configurations as a dataset
-        configs_array = np.array(configs)
-        f.create_dataset("configurations", data=configs_array, compression="gzip")
-        
-        # Store z_table
-        f.create_dataset("z_table", data=z_table, compression="gzip")
-        
+    with h5py.File(h5_path, "w", libver='latest') as f:
         # Store metadata as attributes
         for key, value in metadata.items():
             f.attrs[key] = value
+        
+        # Store z_table as pickled binary
+        z_table_binary = pickle.dumps(z_table)
+        f.create_dataset(
+            "z_table",
+            data=np.frombuffer(z_table_binary, dtype=np.uint8),
+            compression="gzip",
+            compression_opts=4
+        )
+        
+        # Pickle all configurations and concatenate them with size prefixes
+        logger.info("Pickling all configurations...")
+        pickled_configs = []
+        config_sizes = []
+        for config in tqdm(configs, desc="Pickling configurations"):
+            pickled_config = pickle.dumps(config)
+            pickled_configs.append(pickled_config)
+            config_sizes.append(len(pickled_config))
+        
+        # Concatenate all pickled data
+        logger.info("Concatenating pickled data...")
+        all_data = b"".join(pickled_configs)
+        
+        # Store all configurations in a single dataset
+        logger.info("Writing to HDF5...")
+        configs_group = f.create_group("configurations")
+        configs_group.attrs["__len__"] = len(configs)
+        
+        # Store all pickled data as one large array
+        configs_group.create_dataset(
+            "data",
+            data=np.frombuffer(all_data, dtype=np.uint8),
+            compression="gzip",
+            compression_opts=4,
+            chunks=True
+        )
+        
+        # Store offsets for quick access
+        offsets = np.cumsum([0] + config_sizes[:-1], dtype=np.uint64)
+        configs_group.create_dataset("offsets", data=offsets, compression="gzip")
+        configs_group.create_dataset("sizes", data=np.array(config_sizes, dtype=np.uint32), compression="gzip")
         
         logger.info(f"Data stored to {h5_path}")
 
